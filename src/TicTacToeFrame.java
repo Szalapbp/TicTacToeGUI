3ef8fb0a0ba25258011062341adb920bcd31f1ae
Szalapbp <szalapbp@mail.uc.edu>
2025-03-02 03:37:04 -0500

    changed the tictactoeFrame code to include the necessary code to get rid of the pre existing classes per the assignment parameters. appears to be working

--- src/TicTacToeFrame.java
+++ src/TicTacToeFrame.java
@@ -7,7 +7,7 @@ public class TicTacToeFrame extends JFrame
 {
     private static final int ROW = 3;
     private static final int COL = 3;
-    private TicTacToeTile[][] board;
+    private JButton[][] board;
     private String currentPlayer;
 
     public TicTacToeFrame()
@@ -17,20 +17,27 @@ public class TicTacToeFrame extends JFrame
         this.setLayout(new BorderLayout());
         this.currentPlayer = "X";
 
-        JPanel tttPanel = new JPanel(new GridLayout(ROW,COL));
-        board = new TicTacToeTile[ROW][COL];
-        for(int row = 0; row < ROW; row++){
-            for(int col = 0; col < COL; col++){
-                board[row][col] = new TicTacToeTile(row, col);
-                board[row][col].setFont(new Font("Arial", Font.BOLD, 35));
-                board[row][col].setFocusPainted(false);
-                board[row][col].setText(" ");
-                board[row][col].addActionListener(new ButtonClickListener());
-                tttPanel.add(board[row][col]);
+        JPanel tttPanel = new JPanel(new GridLayout(ROW, COL));
+        board = new JButton[ROW][COL];
+
+        for (int row = 0; row < ROW; row++) {
+            for (int col = 0; col < COL; col++) {
+                JButton button = new JButton(" ");
+                button.setFont(new Font("Arial", Font.BOLD, 35));
+                button.setFocusPainted(false);
+
+                button.putClientProperty("row", row);
+                button.putClientProperty("col", col);
+
+                button.addActionListener(new ButtonClickListener());
+                board[row][col] = button;
+                tttPanel.add(button);
             }
         }
 
         this.add(tttPanel, BorderLayout.CENTER);
+
+
 
         JButton quitBtn = new JButton("QUIT");
 
@@ -43,47 +50,83 @@ public class TicTacToeFrame extends JFrame
     }
 
     private class ButtonClickListener implements ActionListener{
+        @Override
         public void actionPerformed(ActionEvent e)
         {
-            TicTacToeTile clickedButton = (TicTacToeTile) e.getSource();
-            if(!clickedButton.getText().equals(" "))
-            {
-                JOptionPane.showMessageDialog(null, "Invalid Move!");
-
+            JButton clickedButton = (JButton) e.getSource();
+            if (!clickedButton.getText().equals(" ")) {
+                JOptionPane.showMessageDialog(null, "Invalid move! Try again.");
+                return;
             }
             clickedButton.setText(currentPlayer);
-            if(isWin(currentPlayer))
-            {
+            if (isWin(currentPlayer)) {
                 JOptionPane.showMessageDialog(null, "Player " + currentPlayer + " wins!");
                 resetBoard();
+                return;
             }
-            if(isTie())
-            {
-                JOptionPane.showMessageDialog(null, "It's a tie!!");
+            if (isTie()) {
+                JOptionPane.showMessageDialog(null, "It's a tie!");
                 resetBoard();
+                return;
             }
-
             currentPlayer = currentPlayer.equals("X") ? "O" : "X";
         }
     }
 
     private boolean isWin(String player)
     {
+        for (int row = 0; row < 3; row++) {
+            if (board[row][0].getText().equals(player) &&
+                    board[row][1].getText().equals(player) &&
+                    board[row][2].getText().equals(player)) {
+                return true;
+            }
+        }
 
-        return false;
+        for (int col = 0; col < 3; col++) {
+            if (board[0][col].getText().equals(player) &&
+                    board[1][col].getText().equals(player) &&
+                    board[2][col].getText().equals(player)) {
+                return true;
+            }
+        }
+
+        if (board[0][0].getText().equals(player) &&
+                board[1][1].getText().equals(player) &&
+                board[2][2].getText().equals(player)) {
+            return true;
+        }
+        return board[0][2].getText().equals(player) &&
+                board[1][1].getText().equals(player) &&
+                board[2][0].getText().equals(player);
     }
 
     private boolean isTie()
     {
-
-        return false;
+        for (int row = 0; row < 3; row++)
+        {
+            for (int col = 0; col < 3; col++)
+            {
+                if (board[row][col].getText().equals(" ")) {
+                    return false;
+                }
+            }
+        }
+        return true;
 
     }
 
     private void resetBoard()
     {
-        for(int row = 0; row < ROW; row++){
-            for(int col = 0; col < COL; col++){
+        int response = JOptionPane.showConfirmDialog(null, "Play another game?", "Game Over", JOptionPane.YES_NO_OPTION);
+        if (response == JOptionPane.NO_OPTION)
+        {
+            System.exit(0);
+        }
+        for (int row = 0; row < ROW; row++)
+        {
+            for (int col = 0; col < COL; col++)
+            {
                 board[row][col].setText(" ");
             }
         }
